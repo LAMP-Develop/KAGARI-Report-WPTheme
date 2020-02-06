3905ff75a677b64d9a2e6f2444b7fc38a132ecd6
ShunyaSuzuki <anyushu2017@gmail.com>
2020-02-06 10:47:12 +0900

    update

--- header.php
+++ header.php
@@ -12,12 +12,29 @@ $wp_url = get_template_directory_uri(); ?>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-79537153-10"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-79537153-10');
-  gtag('config', 'AW-672878991');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-79537153-10');
+gtag('config', 'AW-672878991');
 </script>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1424654571021609');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1424654571021609&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
 <?php endif; ?>
 </head>
 <body>
